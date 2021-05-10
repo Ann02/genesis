e050056304f54061b33468525cdacc645025764d
Kiva Vlad <vladkiva9@gmail.com>
2021-05-10 14:56:09 +0300

    added email validator, localStorage, fix some bugs

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
+const form = document.querySelector('#form'),
+    formEmail = document.querySelector('#email'),
+    formBtn = document.querySelector('#form-btn');
+
 setInterval(updateClock, 1000);
+
 function updateClock() {
     const clock = document.querySelector('.clock');
     clock.innerText = new Date().toLocaleTimeString();
 }
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]{3,}\.)+[a-zA-Z]{3,3}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function checkValidate() {
+    if (validateEmail(formEmail.value)) {
+        formBtn.removeAttribute("disabled");
+    } else {
+        formBtn.setAttribute("disabled", "true");
+    }
+}
+
 class Carousel {
     constructor(images) {
         this.images = images;
@@ -34,3 +53,35 @@ new Carousel([
     'img/Genesisin.jpg',
     'img/Genesiscrystal.jpg'
 ]);
+
+if (localStorage.getItem('name') !== null) {
+    form.querySelector('#name').value = localStorage.getItem('name');
+    form.querySelector('#number').value = localStorage.getItem('number');
+    form.querySelector('#email').value = localStorage.getItem('email');
+}
+
+formBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    console.log(validateEmail(form.querySelector('#email').value));
+    const formData = new FormData(form);
+
+    localStorage.setItem('name', formData.get('name'));
+    localStorage.setItem('number', formData.get('number'));
+    localStorage.setItem('email', formData.get('email'));
+});
+
+formBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    localStorage.setItem('name', formData.get('name'));
+    localStorage.setItem('number', formData.get('number'));
+    localStorage.setItem('email', formData.get('email'));
+});
+
+formEmail.addEventListener("input", () => {
+    checkValidate();
+});
+
+checkValidate();
